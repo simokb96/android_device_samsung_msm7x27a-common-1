2c6f1ea7f72fedae36a286bd9facf9fff45f2f93
Nothing-Dev <alfredomartinez_1423@hotmail.com>
2016-03-28 15:53:44 -0430

    Add CSR 7820 things
    Add patch used in external_bluetooth_bluez to fix initialization timed out
    Copy BCCMD from vendor
    
    Signed-off-by: Nothing-Dev <alfredomartinez_1423@hotmail.com>

--- prebuilt/bluetooth/init.bt.sh
+++ prebuilt/bluetooth/init.bt.sh
@@ -1,7 +1,8 @@
 #!/system/bin/sh
 #daniel_hk
+#Modified log messages (NV-Dev)
 
-LOG_TAG="qcom-bluetooth"
+LOG_TAG="BT-CSR7820"
 BTUART_PORT=/dev/ttyHS0
 BTSTATE=/sys/class/rfkill/rfkill0/state
 PSCONFIG=/system/etc/PSConfig_7820.psr
@@ -22,22 +23,23 @@ failed ()
   exit $2
 }
 
-#Enable power of csr chip
+#Enable power of CSR chip
+logi "Enable BT Power"
 echo "1" > $BTSTATE
 
 # PS Config with bccmd
 logwrapper /system/bin/bccmd -t bcsp -d $BTUART_PORT -b 115200 psload -r $PSCONFIG
 case $? in
-  0) logi "bccmd init port....done";;
-  *) failed "port: $BTUART_PORT - bccmd failed" $?;
+  0) logi "CSR BCCMD initialization done.";;
+  *) failed "CSR BCCMD initialization failed. Port initialization failure: $BTUART_PORT" $?;
      exit $?;;
 esac
 
-# attach HCI 
+# Attach HCI 
 logwrapper /system/bin/hciattach -p $BTUART_PORT bcsp 3000000 flow
 case $? in
-  0) logi "hci attached to : $BTUART_PORT";;
-  *) failed "port: $BTUART_PORT - hciattach failed" $?;
+  0) logi "HCI attach done. Attached to port: $BTUART_PORT";;
+  *) failed "HCI attach failed. Failed to attach to port: $BTUART_PORT - " $?;
      exit $?;;
 esac
 
